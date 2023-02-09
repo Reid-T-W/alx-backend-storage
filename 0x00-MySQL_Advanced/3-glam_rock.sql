618e91a6c1b96dfb871352b238865aebbaba8d7b
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 16:11:41 +0300

    makes use of case statement

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,9 @@
 -- List all bands with Glam rock as their style
 SELECT band_name,
-	IFNULL(split, 2022) - IFNULL(formed, 0)
+	CASE
+		WHEN split IS NULL THEN (2022 - formed)
+		ELSE (split - formed)
+	END
 AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
