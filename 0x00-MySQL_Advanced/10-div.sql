5bc5f8554e5a39ea52a471e969fee6251847cbfd
Reid-T-W <rediettadesse100@gmail.com>
2023-02-10 00:54:49 +0300

    mysql function the performs division

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,15 @@
+-- Function that performs division
+DROP FUNCTION SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv ( a INT, b INT)
+RETURNS FLOAT(16)
+DETERMINISTIC
+BEGIN
+	IF b = 0 THEN
+		RETURN 0;
+	ELSE
+		RETURN a/b;
+	END IF;
+END; $$
+
+DELIMITER ;
