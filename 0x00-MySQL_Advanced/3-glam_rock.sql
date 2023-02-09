443618759031fda6c27ac626df3d2354f95a2419
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 16:00:33 +0300

    first line comment shortened

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- SQL scrip that list all bands with Glam rock as their main styleranked by their longevity
+-- List all bands with Glam rock as their style
 SELECT band_name, 
 	IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
