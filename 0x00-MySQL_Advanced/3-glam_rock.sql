d231b5f558fde830bd62ce4256219baa7d04c833
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 11:14:31 +0300

    cases where bands have not split handled

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,10 @@
 -- SQL scrip that list all bands with Glam rock as their main styleranked by their longevity
-SELECT band_name, (split - formed) as lifespan FROM metal_bands
+SELECT band_name, 
+CASE
+	WHEN split is NULL THEN (2020 - formed)
+	ELSE (split - formed)
+END
+AS lifespan
+FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
