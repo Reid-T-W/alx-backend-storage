f02d397909a888aff4b9ea297e75c8a60350ef5c
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 15:24:27 +0300

    Email validation

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,11 @@
+-- Trigger executed upon inserting into
+-- the orders table
+USE holberton
+DROP TRIGGER IF EXISTS reset_valid_email;
+CREATE TRIGGER reset_valid_email BEFORE UPDATE on users
+FOR EACH ROW
+SET NEW.email = NEW.email, NEW.valid_email = 
+CASE
+	WHEN OLD.email != NEW.email THEN 0
+	ELSE 1
+END
