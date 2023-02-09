a42610235bb32df5813874b279fa090b141919be
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 16:18:27 +0300

    All sql syntax changed to upper case

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- Trigger executed upon inserting into
 -- the orders table
 DROP TRIGGER IF EXISTS reset_valid_email;
-CREATE TRIGGER reset_valid_email BEFORE UPDATE on users
+CREATE TRIGGER reset_valid_email BEFORE UPDATE ON users
 FOR EACH ROW
 SET NEW.email = NEW.email, NEW.valid_email = 
 CASE
