ec587f503312f3afe50c2bf144eff024e07b029e
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 13:32:16 +0300

    Uses trigger to update values of a second table

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,9 @@
+-- Trigger executed upon inserting into
+-- the orders table
+USE holberton
+DROP TRIGGER IF EXISTS update_item_count;
+CREATE TRIGGER update_item_count AFTER INSERT on orders
+FOR EACH ROW
+UPDATE items
+SET items.quantity = items.quantity - NEW.number
+WHERE items.name = NEW.item_name
