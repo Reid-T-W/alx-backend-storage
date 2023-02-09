ba3b18c07e4e40a49f27b0a8f9313d82a086584c
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 15:58:37 +0300

    Null check for formed added

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- SQL scrip that list all bands with Glam rock as their main styleranked by their longevity
 SELECT band_name, 
-	IFNULL(split, 2020) - formed AS lifespan
+	IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC, band_name DESC;
