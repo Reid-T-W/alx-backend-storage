f90ae236ff95fbc1c497dd090f45f7f00e5c79c3
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 11:40:28 +0300

    comment split into two lines

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
--- SQL scrip that list all bands with Glam rock as their main styleranked by their longevity
+-- SQL scrip that list all bands with Glam rock
+-- as their main styleranked by their longevity
 SELECT band_name, 
 CASE
 	WHEN split is NULL THEN (2020 - formed)
