713bf83734d6e155e9fd3c5f2e6a4a2b09d697ca
Reid-T-W <rediettadesse100@gmail.com>
2023-02-09 16:04:00 +0300

    Trailing whitespace removed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- List all bands with Glam rock as their style
-SELECT band_name, 
+SELECT band_name,
 	IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
