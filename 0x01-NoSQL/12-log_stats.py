14165b3f52849289206de57f68163f9526ddae6a
Reid-T-W <rediettadesse100@gmail.com>
2023-02-14 08:40:24 -0800

    script will not execute when imported

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,21 +6,22 @@ MonogDB
 import pymongo
 
 
-# Establishing connection to the database
-client = pymongo.MongoClient("mongodb://localhost:27017/")
+if __name__ == "__main__":
+    # Establishing connection to the database
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-# Getting db
-db = client["logs"]
+    # Getting db
+    db = client["logs"]
 
-# Collection name
-col = db["nginx"]
+    # Collection name
+    col = db["nginx"]
 
-print(f"{col.count_documents({})} logs")
-print("Methods:")
-print(f"    method GET: {col.count_documents({'method': 'GET'})}")
-print(f"    method POST: {col.count_documents({'method': 'POST'})}")
-print(f"    method PUT: {col.count_documents({'method': 'PUT'})}")
-print(f"    method PATCH: {col.count_documents({'method': 'PATCH'})}")
-print(f"    method DELETE: {col.count_documents({'method': 'DELETE'})}")
-print("{} status check".
-      format(col.count_documents({'method': 'GET', 'path': '/status'})))
+    print(f"{col.count_documents({})} logs")
+    print("Methods:")
+    print(f"    method GET: {col.count_documents({'method': 'GET'})}")
+    print(f"    method POST: {col.count_documents({'method': 'POST'})}")
+    print(f"    method PUT: {col.count_documents({'method': 'PUT'})}")
+    print(f"    method PATCH: {col.count_documents({'method': 'PATCH'})}")
+    print(f"    method DELETE: {col.count_documents({'method': 'DELETE'})}")
+    print("{} status check".
+          format(col.count_documents({'method': 'GET', 'path': '/status'})))
