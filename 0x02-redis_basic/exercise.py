de5d2edc47f4f91f0a2d4a53c6e39a5dbeee395d
Reid-T-W <rediettadesse100@gmail.com>
2023-02-15 23:59:15 +0300

    bytes converted to int and string

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Write strings to Redis """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache():
@@ -17,3 +17,28 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get_str(self, key):
+        """Convert byte to string"""
+        return(self._redis.get(key).decode("utf-8"))
+
+    def get_int(self, key):
+        """Convert byte to int"""
+        return(int(self._redis.get(key), 10))
+
+    def get(self, key: str, fn: Callable = None):
+        """
+        Modified get inorder to convert the values
+        to their original types
+        """
+        # Handling the case where key does not exist
+        # the default behaviour of get is conserved
+        if not self._redis.exists(key):
+            return self._redis.get(key)
+        else:
+            if fn == int:
+                return(self.get_int(key))
+            elif fn is None:
+                return(key)
+            else:
+                return(self.get_str(key))
