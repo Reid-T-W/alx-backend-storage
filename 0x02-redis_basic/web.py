932f567b06042f7f75460c3390daab124c3913aa
Reid-T-W <rediettadesse100@gmail.com>
2023-02-16 22:41:03 +0300

    name of key changed to count

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -11,7 +11,9 @@ def get_page(url: str) -> str:
     Retrieves an html page given a url
     """
     cache = redis.Redis()
-    key = f"count:{url}"
+    # key = f"count:{url}"
+    key = "Count"
+    print(cache.get(key))
     result = requests.get(url).text
     cache.incr(key)
     cache.setex(url, 10, result)
