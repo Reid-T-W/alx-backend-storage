7f29369d94507d18ccc1dd0f817a6f56a5719537
Reid-T-W <rediettadesse100@gmail.com>
2023-02-16 22:12:18 +0300

    decorator removed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,30 +4,16 @@ Tracks the number of times a url is called
 """
 import requests
 import redis
-from functools import wraps
 
 
-def track_url_access_count(method):
-    """
-    counts the number of times a url is accessed
-    """
-    @wraps(method)
-    def wrapper(*args, **kwds):
-        cache = redis.Redis()
-        key = f"count:{args[0]}"
-        result = method(*args, **kwds)
-        # cache.expire(key, 10)
-        cache.setex(key, 10, result)
-        # cache.incr(key)
-        print(cache.get(key))
-        return result
-    return wrapper
-
-
-@track_url_access_count
 def get_page(url: str) -> str:
     """
     Retrieves an html page given a url
     """
-    html = requests.get(url).text
-    return html
+    cache = redis.Redis()
+    key = f"count:{url}"
+    result = requests.get(url).text
+    # cache.setex(key, 10, result)
+    cache.incr(key)
+    print(cache.get(key))
+    return result
